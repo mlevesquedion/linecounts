69612b41049efdeb409e504dab895405dfa371e9
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2018-07-21 14:18:41 -0400

    First commit

---
+++ lib/linecounts.ex
@@ -0,0 +1,18 @@
+defmodule Linecounts do
+  @moduledoc """
+  Documentation for Linecounts.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Linecounts.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/linecounts_test.exs
@@ -0,0 +1,8 @@
+defmodule LinecountsTest do
+  use ExUnit.Case
+  doctest Linecounts
+
+  test "greets the world" do
+    assert Linecounts.hello() == :world
+  end
+end
